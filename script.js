edc65f0a591951b0e7b92b114626c64fb2491417
Rod Blanco <rodrigo.a.blanco1990@gmail.com>
2021-04-22 21:47:40 -0700

    bad changes

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://alpha-vantage.p.rapidapi.com/query?from_currency=BTC&function=CURRENCY_EXCHANGE_RATE&to_currency=USD",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "6e2a194552msh708477475f34fd7p1cb828jsnac3379c4cc7d",
-		"x-rapidapi-host": "alpha-vantage.p.rapidapi.com"
-	}
-};
-
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
-
-// need to create localStorage
+g// need to create localStorage
 //need to link stocks with save buttons
 // need to create remove button
 // create search bar function
